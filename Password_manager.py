70dfcf420bbb52ddee6912228302838def3b5c28
Hidden-name# <joscarle33@gmail.com>
2024-12-05 20:04:04 +0100

    Password_manager.py

--- Password_manager.py
+++ Password_manager.py
@@ -1,19 +1,19 @@
 import string
 import random
 import json
-import create_settings_file
-import Verify_Password_Security
+import create_settings_file        # The second python file
+import Verify_Password_Security    # The third python file
 
-class Password_Manager:
-    def __init__(self):
+class Password_Manager:    # Class Password Manager
+    def __init__(self):        # Initialize 5 attributes
         self.asscii_digits = string.digits
         self.asscii_upper_chars = string.ascii_uppercase
         self.asscii_lower_chars = string.ascii_lowercase
         self.asscii_especial_chars = string.punctuation
-        self.save = "D:\\Visual Studio Code\\Python\\Password Manager\\Passwords.txt"
+        self.save = "D:\\Visual Studio Code\\Python\\Password Manager\\Passwords.txt"       # The path of the passwords file (Where passwords save)
     
     def create_password(self, settings):
-        self.settings = settings
+        self.settings = settings        # The atribute of the settings file that load the settings (in the create_settings_file.py)
 
         self.length = self.settings["password_length"]
         self.upper_chars = self.settings["has_upper_chars"]
@@ -46,18 +46,18 @@ class Password_Manager:
 
         password = "".join(random.choice(password_chars) for i in range(self.length))
         if Verify_Password_Security.verify_password_security(password) == "WEAK" or Verify_Password_Security.verify_password_security(password) == "SO WEAK" or Verify_Password_Security.verify_password_security(password) == "MID":
-            print("\nPassword isn't safe!!\n")
+            print("\nPassword isn't safe!!\n")        # To make passwords secure
             improve = True if input("Do you want to improve password security? (y/n): ").lower() == "y" else False
             if improve:
                 print("\n")
-                suggest = Verify_Password_Security.suggest_safe_password()
+                suggest = Verify_Password_Security.suggest_safe_password()    # Suggest a password
                 print(f"\nPassword suggested is: {suggest}\n")
                 if input("Do you want to continue? (y/n): ").lower() == "y":
                     password = suggest
                     print("\n")
         print("\nPassword created successfully")
         print(f"Password created is: {password}\n")
-        with open(self.save, "a") as file:
+        with open(self.save, "a") as file:    # Save password in the passwords file (atribute self.save reference)
             file.write(password)
 
     def view_all_passwords(self):
@@ -71,9 +71,9 @@ class Password_Manager:
 
     def modify_password(self, identifier):
         with open(self.save, "r") as file:
-            lines = file.readlines()
+            lines = file.readlines()        # Read lines before to write
 
-        if identifier < 0 or identifier >= len(lines):
+        if identifier < 0 or identifier >= len(lines):    # Identifier is line where the specific password was located
             print("Invalid identifier. Try again\n")
             return
         else:
@@ -91,31 +91,31 @@ class Password_Manager:
 
         with open(self.save, "w") as file:
             lines[identifier] = new_password
-            file.writelines(lines)
+            file.writelines(lines)        # Overwrite the file with the new password
         print("Password changed successfully\n")
         print(f"New password is: {new_password}\n")
     
     def delete_password(self, identifier):
         with open(self.save, "r") as file:
-            lines = file.readlines()
+            lines = file.readlines()        # Read lines before write
             if identifier < 0 or identifier > len(lines):
                 print("Invalid identifier. Try again\n")
                 return
         
-        with open(self.save, "w") as file:
+        with open(self.save, "w") as file:        # Overwrite file without the old password
             overwrite_file = [line for index, line in enumerate(lines) if index != identifier]
             file.writelines(overwrite_file)
         print("Password deleted successfully\n")
 
-def main():
-    create_settings_file.create_settings_file()
-    password_manager = Password_Manager()
+def main():        # The main function
+    create_settings_file.create_settings_file()    # Create the settings file (watch create_settings_file.py)
+    password_manager = Password_Manager()        # Initialize the object "password_manager"
     settings_path_file = "D:\\Visual Studio Code\\Python\\Password Manager\\Settings.json"
     while True:
         print(
             "What do you want to do?\n"
             "1. Create a password\n"
-            "2. View all the passwords created\n"
+            "2. View all the passwords created\n"        # The options of the program
             "3. Modify a password\n"
             "4. Delete a password\n"
             "5. Quit\n"
@@ -123,7 +123,7 @@ def main():
         while True:
             try:
                 choose_option = int(input("Choose an option (1-5): "))
-                if choose_option < 1 or choose_option > 5:
+                if choose_option < 1 or choose_option > 5:        # If the value not match the range try again
                     print("\nValue is lower or higher. Please try again\n")
                 else:
                     break
@@ -134,7 +134,7 @@ def main():
                 continue
             except KeyboardInterrupt:
                 print("\n")
-                print("Operation cancelled successfully\n")
+                print("Operation cancelled successfully\n")        # If user press ctrl + c (equals KeyboardIterrupt) cancel the operation in course
                 print("\n")
                 print(
                     "What do you want to do?\n"
@@ -152,13 +152,13 @@ def main():
                 try:
                     with open(settings_path_file, "r") as file:
                         settings_file = json.load(file)
-                except json.JSONDecodeError:
+                except json.JSONDecodeError:        # Sometimes the json file might not load correctly
                     print("An error had ocurred during executing json file. Please, try again later\n")
                     return
                 password_manager.create_password(settings_file)
     
             elif choose_option == 2:
-                print(" PASSWORDS! ".center(30, "-") + "\n")
+                print(" PASSWORDS! ".center(30, "-") + "\n")    # Center the text to look like this    ---------- PASSWORDS! ---------
                 password_manager.view_all_passwords()
                 print("\n")
         
@@ -181,7 +181,7 @@ def main():
                 except ValueError:
                     print("Invalid input. Try again\n")
                 print("\n")
-                file_delete = identifier - 1
+                file_delete = identifier - 1        # Because the first line is 0 for that cases subtract value in 1
                 password_manager.delete_password(file_delete)
 
             elif choose_option == 5:
